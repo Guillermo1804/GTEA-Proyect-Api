fc0240724cea1ff586b3f860fe19490c0048b381
Guillermo1804 <144832378+Guillermo1804@users.noreply.github.com>
2026-02-16 20:59:25 -0600

    agregación de la base de datos
    Solamente agregué la base de datos que vamos a ocupar de phpmyadmin

--- GTEA_Project_API/settings.py
+++ GTEA_Project_API/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'GTEA_Project_API.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gtea_proyecto_api',  # El nombre exacto que pusiste en phpMyAdmin
+        'USER': 'root',               # Usuario por defecto de XAMPP
+        'PASSWORD': '',               # En XAMPP, el root suele no tener contraseña
+        'HOST': '127.0.0.1',          # O 'localhost'
+        'PORT': '3306',               # Puerto por defecto de MySQL
     }
 }
 
