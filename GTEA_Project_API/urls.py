c49900d07c2516ba4cc19476fccdfd4c3f16fd16
Guillermo1804 <memosanchez101@gmail.com>
2026-05-26 01:49:52 -0600

    acut

--- GTEA_Project_API/urls.py
+++ GTEA_Project_API/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
 
     # Admins
     path('admins/', users.AdminAll.as_view(), name='admins-list'),
-    path('admins/detail/', users.AdminView.as_view(), name='admins-detail'),
     path('admins/edit/', users.AdminsViewEdit.as_view(), name='admins-edit'),
     path('admins/<int:pk>/', users.AdminIsActivePatch.as_view(), name='admins-patch-active'),
 
